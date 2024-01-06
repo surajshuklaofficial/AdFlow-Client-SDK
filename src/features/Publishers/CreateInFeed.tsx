f45e56a229b4da2e80c6db420583ad0f01bc234d
Your Name <your_email@example.com>
2024-01-06 22:11:30 +0530

    deployment-3

--- src/features/Publishers/CreateInFeed.tsx
+++ src/features/Publishers/CreateInFeed.tsx
@@ -6,14 +6,12 @@ import { Button } from "../../components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "../../components/ui/dialog";
 import { Input } from "../../components/ui/input";
-import { Label } from "../../components/ui/label";
 import {
   Tabs,
   TabsContent,
